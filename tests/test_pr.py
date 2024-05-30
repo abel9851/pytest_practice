8b1ab4ced46572fe2ab1280f6a6e6d125f758b0e
HeejunShin <66953834+abel9851@users.noreply.github.com>
2024-05-31 00:18:28 +0900

    test: use xfail pytest -rx

--- tests/test_pr.py
+++ tests/test_pr.py
@@ -6,6 +6,7 @@ def test_pr_functions_as_expected():
     assert True
 
 @mark.enter
-@mark.skip(reason="fail case")
+# @mark.skip(reason="fail case")
+@mark.xfail(reason="just fail test")
 def test_pr_fails():
     assert False
